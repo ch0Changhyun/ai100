fb5d897bd0075fe183ef750169fe644a33866770
ch0Changhyun <98530076+ch0Changhyun@users.noreply.github.com>
2024-06-15 17:17:44 +0900

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title('동물 이미지 😊')
-title = st.text_input('영어로 동물 이름 입력: ')
-
-if st.button('찾아보기'):
-    url = 'https://edu.spartacodingclub.kr/random/?' + title
-    st.image(url)
